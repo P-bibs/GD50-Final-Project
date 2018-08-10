da2bc8aeedebdfe5e29f5aef8eec8ca2bb785f61
Paul <biberstein.paul@gmail.com>
2018-08-10 08:09:46 -0700

    Redesigned player jump amount storage

--- src/Player.lua
+++ src/Player.lua
@@ -7,12 +7,8 @@ Player = Class{__includes = Entity}
 function Player:init(def, x, y)
     Entity.init(self, def, x, y)
 
-    self.jumps = {}
-    for i = 1, PLAYER_MAX_JUMPS do
-        table.insert(self.jumps, {
-            status = 'ready'
-        })
-    end
+    self.jumps = PLAYER_MAX_JUMPS
+    self.brokenJumps = 0
 
     self.hitbox = nil
     self.attackAnim = nil
@@ -91,7 +87,7 @@ function Player:update(dt)
     end
 
     --jump
-    if love.keyboard.wasPressed(KEY_JUMP) and self:getJumps() > 0 then
+    if love.keyboard.wasPressed(KEY_JUMP) and self.jumps > 0 then
         self.vy = PLAYER_JUMP_VELOCITY
         self:alterJumps(-1)
         gSounds['player-jump']:play()
@@ -135,42 +131,15 @@ function Player:update(dt)
     end
 end
 
---function that alters the number of jumps the player has remaining, either adding or subtracting based on sign
---this has to be done intelligently because jump slots can become temporarily unusable
 function Player:alterJumps(amount)
-    --figure out the current highest jump slot thats ready (not broken or used)
-    local currentIndex = 0
-    for i = 1, #self.jumps do
-        if self.jumps[i].status == 'ready' then
-            currentIndex = i
-        end
+    --if altering by a positive amount, add the amount, not going above max jumps, and ten subtract any broken jumps
+    if amount > 0 then
+        self.jumps = math.min(PLAYER_MAX_JUMPS, self.jumps + amount)
+        self.jumps = math.min(self.jumps, self.jumps - self.brokenJumps)
+    --if subtracting jumps, make sure jumps is never less than 0
+    elseif amount < 0 then
+        self.jumps = math.max(0, self.jumps + amount)
     end
-
-    --if the funciton call was to subtract jumps, start at the index calculated above and remove it as well as
-    --those below it depending on the amount passed in
-    if amount < 0 then
-        for i = currentIndex, math.max(1, currentIndex + amount + 1), -1 do
-            self.jumps[i].status = 'used'
-        end
-    else 
-    --if the funciton call was positive, start at the index above the current index and set it to ready,
-    --unless it is broken
-        for i = currentIndex + 1, math.min(PLAYER_MAX_JUMPS, currentIndex + amount), 1 do
-            if self.jumps[i].status ~= 'broken' then 
-                self.jumps[i].status = 'ready'
-            end
-        end
-    end
-end
-
---get current number of usable jumps
-function Player:getJumps()
-    for i = 1, #self.jumps do
-        if self.jumps[i].status ~= 'ready' then
-            return i - 1
-        end
-    end
-    return #self.jumps
 end
 
 function Player:render()
@@ -183,18 +152,16 @@ function Player:render()
     love.graphics.draw(gTextures[self.currentAnimation.texture], gFrames[self.currentAnimation.texture][self.currentAnimation:getCurrentFrame()],
         math.floor(self.x) + 8, math.floor(self.y) + 10, 0, 1, 1, 8, 10)
 
-    for i = 1, #self.jumps do
-        if self.jumps[i].status == 'ready' then
+    --draw jumps indicator
+    for i = 1, PLAYER_MAX_JUMPS do
+        if i <= self.jumps then
             love.graphics.setColor(255, 255, 255, 255)
             love.graphics.circle('fill', math.floor(self.x  - 3 + 3 * i), math.floor(self.y), 1)
-        end
-        if self.jumps[i].status == 'used' then
-            love.graphics.setColor(100, 100, 100, 100)
-            love.graphics.circle('fill', math.floor(self.x  - 3 + 3 * i), math.floor(self.y), 1)
-        end
-        --if i <= self.jumps then
-        if self.jumps[i].status == 'broken' then
+        elseif i > PLAYER_MAX_JUMPS - self.brokenJumps then
             love.graphics.setColor(255, 0, 0, 255)
+            love.graphics.circle('fill', math.floor(self.x  - 3 + 3 * i), math.floor(self.y), 1)
+        else
+            love.graphics.setColor(100, 100, 100, 100)
             love.graphics.circle('fill', math.floor(self.x  - 3 + 3 * i), math.floor(self.y), 1)
         end
     end
