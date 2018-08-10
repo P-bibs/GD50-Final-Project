ee1fc0ba6d87774d9a44788d152e7e8efbf18556
Paul <biberstein.paul@gmail.com>
2018-08-10 08:54:23 -0700

    Update constants

--- src/constants.lua
+++ src/constants.lua
@@ -20,7 +20,7 @@ SCREEN_TILE_HEIGHT = VIRTUAL_HEIGHT / TILE_SIZE
 -- camera scrolling speed
 CAMERA_SPEED = 100
 
--- player walking speed
+-- player acceleration speed
 PLAYER_WALK_SPEED = 10
 
 -- player jumping velocity
@@ -31,9 +31,9 @@ PLAYER_JUMP_VELOCITY = 200
 --
 TILE_ID_EMPTY = 40
 TILE_ID_GROUND = 362
-
 TILE_ID_TOP = 326
 
+--texture used if an object is instantiated with a nil texture
 ERROR_ANIM = {
     ['error'] = {
     frames = {1},
@@ -53,10 +53,13 @@ KEY_ATTACK_DOWN = 'down'
 
 PI = math.pi
 
+--how close the player has to get to a bug enemy before they will leave idle state
 BUG_ALERT_DISTANCE = 200
 
+--how long enemies and the player freeze when hit
 FREEZE_DURATION = 0.1
 
+--the max number of jumps the player can have at once
 PLAYER_MAX_JUMPS = 6
 
 KNOCKBACK = 100
@@ -66,4 +69,5 @@ To progress, you must advance upward, but you're 6 jumps will only get you so fa
 
 HOW_TO_TEXT2 = [[Attempt to make youre way through 2 challenging levels of ascent through harrowing pits, and then pit yourself against a new foe in level 3. To best him, you will have to be creative in your fighting tactics. (Maybe there is a way to stun him?). Once you have completed every level, try to do them again, but faster! You're fastest times are recorded on the leaderboard. Now go! Adventure!]]
 
+--Number of fireballs the boss shoots
 NUMBER_FIREBALLS = 8
